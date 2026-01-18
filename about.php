0f6af228e65162f19d998a5d02dbbde03949fc42
Gabrielb-Webdev <gabrielb.webdev@gmail.com>
2026-01-18 18:24:13 -0300

    feat: Rediseñar la sección "Acerca de" con nuevos estilos, marcadores de posición y mejoras en la presentación

--- about.php
+++ about.php
@@ -9,47 +9,22 @@ include __DIR__ . '/includes/header.php';
 <!-- About Page -->
 <section class="about-page-section">
     <div class="container">
-        <h2 class="section-title centered">ABOUT US</h2>
-        
-        <div class="about-page-image">
-            <div class="vitamins-placeholder">
-                <div class="vitamin-bottle vitamin-c">
-                    <div class="bottle-cap"></div>
-                    <div class="bottle-body">
-                        <div class="bottle-label">
-                            <span class="vitamin-name">VITAMIN</span>
-                            <span class="vitamin-letter">C</span>
-                        </div>
-                    </div>
-                    <div class="pills-scattered">
-                        <span class="pill pill-orange"></span>
-                        <span class="pill pill-orange"></span>
-                        <span class="pill pill-orange"></span>
-                    </div>
-                </div>
-                <div class="vitamin-bottle vitamin-b12">
-                    <div class="bottle-cap"></div>
-                    <div class="bottle-body">
-                        <div class="bottle-label">
-                            <span class="vitamin-name">VITAMIN</span>
-                            <span class="vitamin-letter">B<sub>12</sub></span>
-                        </div>
-                    </div>
-                    <div class="pills-scattered">
-                        <span class="pill pill-yellow"></span>
-                        <span class="pill pill-yellow"></span>
-                        <span class="pill pill-yellow"></span>
-                    </div>
-                </div>
+        <div class="about-page-content">
+            <h2 class="section-title centered animate-fade-in">ABOUT US</h2>
+            
+            <div class="about-page-image animate-slide-up">
+                <img src="https://plantillashtmlgratis.com/wp-content/themes/helium-child/vista_previa/page279/medion/images/about-medicine.png" 
+                     alt="Vitamin C and B12 bottles" 
+                     class="vitamins-image">
             </div>
-        </div>
-        
-        <div class="about-page-text">
-            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it</p>
-        </div>
-        
-        <div class="about-page-button">
-            <a href="<?php echo BASE_URL; ?>/products.php" class="btn-read-more">Read More</a>
+            
+            <div class="about-page-text animate-fade-in-delay">
+                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it</p>
+            </div>
+            
+            <div class="about-page-button animate-fade-in-delay-2">
+                <a href="<?php echo BASE_URL; ?>/products.php" class="btn-read-more">Read More</a>
+            </div>
         </div>
     </div>
 </section>
